8aeb7c63612877c57b4c8d9f6bb6150e0360c78a
DieCyberwerkstatt <seslija.sasha@gmail.com>
2023-06-09 23:35:46 +0200

    initialized firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'fir-todolist-41ac9',
+    appId: '1:654890468289:web:a12228a116342086512540',
+    storageBucket: 'fir-todolist-41ac9.appspot.com',
+    apiKey: 'AIzaSyAJJMGNmtxG4xQNfKKkgICFKITh-T4BlKY',
+    authDomain: 'fir-todolist-41ac9.firebaseapp.com',
+    messagingSenderId: '654890468289',
+    measurementId: 'G-6DQG800KBZ',
+  },
+};
